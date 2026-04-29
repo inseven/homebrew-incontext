c86587173e7948950c34e0279a6461afac743b74
jbmorley <220887+jbmorley@users.noreply.github.com>
2026-04-29 22:39:29 +0000

    Update formula

--- Formula/incontext.rb
+++ Formula/incontext.rb
@@ -2,8 +2,8 @@ class Incontext < Formula
 
   desc "Static website builder for macOS"
   homepage "https://github.com/inseven/incontext-waialua"
-  url "https://github.com/inseven/incontext/releases/download/3.9.20/incontext-3.9.20-260425052110733909.zip"
-  sha256 "69a2d559aefdac00f5a941f1259bcc3aef1d2ce2938808b1f3065e2e3b2d256e"
+  url "https://github.com/inseven/incontext/releases/download/3.10.0/incontext-3.10.0-260429223309248713.zip"
+  sha256 "5400d62298572f08bac141175a67c2811c3cfa14f56af0d4325f7d11da65afae"
 
   def install
     bin.install Dir["./*"]
